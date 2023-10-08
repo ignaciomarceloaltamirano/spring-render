59aa9c061e3717ecd4098f8d0f28e7391e47f9fd
ignacio <ignacioaltamiranodev@gmail.com>
2023-10-08 20:22:00 -0300

    updated cors filter 2

---
+++ src/main/java/com/docker/mysql/security/config/CorsConfig.java
@@ -0,0 +1,40 @@
+package com.docker.mysql.security.config;
+
+import org.apache.catalina.filters.CorsFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+@Configuration
+public class CorsConfig {
+        @Bean
+        public CorsFilter corsFilter() {
+            UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+            CorsConfiguration corsConfig = new CorsConfiguration();
+            corsConfig.setAllowCredentials(true);
+            corsConfig.addAllowedOrigin("*");
+            corsConfig.setAllowedHeaders(Arrays.asList(
+                    HttpHeaders.AUTHORIZATION,
+                    HttpHeaders.ACCEPT,
+                    HttpHeaders.SET_COOKIE,
+                    HttpHeaders.CONTENT_TYPE
+            ));
+            corsConfig.setAllowedMethods(Arrays.asList(
+                    HttpMethod.HEAD.name(),
+                    HttpMethod.GET.name(),
+                    HttpMethod.POST.name(),
+                    HttpMethod.PUT.name(),
+                    HttpMethod.PATCH.name(),
+                    HttpMethod.DELETE.name(),
+                    HttpMethod.OPTIONS.name()
+            ));
+            corsConfig.setMaxAge(3600L);
+            source.registerCorsConfiguration("/**", corsConfig);
+            return new CorsFilter();
+        }
+}
