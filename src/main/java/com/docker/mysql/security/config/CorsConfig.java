92ea5aad95419cdeea4ba3da79c1ba83e556e1c1
ignacio <ignacioaltamiranodev@gmail.com>
2023-10-08 21:00:37 -0300

    updated cors filter 6

--- src/main/java/com/docker/mysql/security/config/CorsConfig.java
+++ src/main/java/com/docker/mysql/security/config/CorsConfig.java
@@ -1,8 +1,10 @@
 package com.docker.mysql.security.config;
 
 import org.apache.catalina.filters.CorsFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.cors.CorsConfiguration;
@@ -13,17 +15,16 @@ import java.util.Arrays;
 @Configuration
 public class CorsConfig {
         @Bean
-        public CorsFilter corsFilter() {
+        public FilterRegistrationBean<CorsFilter> corsFilter() {
             UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
             CorsConfiguration corsConfig = new CorsConfiguration();
             corsConfig.setAllowCredentials(true);
-//            corsConfig.addAllowedOrigin("*");
             corsConfig.addAllowedOrigin("http://localhost:5173");
             corsConfig.setAllowedHeaders(Arrays.asList(
                     HttpHeaders.AUTHORIZATION,
                     HttpHeaders.ACCEPT,
                     HttpHeaders.SET_COOKIE,
-                    HttpHeaders.CONTENT_TYPE
+                    HttpHeaders.CONTENT_TYPE,
             ));
             corsConfig.setAllowedMethods(Arrays.asList(
                     HttpMethod.HEAD.name(),
@@ -36,6 +37,8 @@ public class CorsConfig {
             ));
             corsConfig.setMaxAge(3600L);
             source.registerCorsConfiguration("/**", corsConfig);
-            return new CorsFilter();
+            FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
+            bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+            return bean;
         }
 }
